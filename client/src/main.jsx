0e2fbd6f2b1e94ee6738abd06e8f76f47a823a4c
intelligent-psychopath <79144740+intelligent-psychopath@users.noreply.github.com>
2023-02-16 16:57:44 +0530

    Switched Base Url

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,8 +4,8 @@ import axios from "axios";
 import App from "./App";
 import "./index.css";
 
-axios.defaults.baseURL = "http://localhost:3000/api/v1";
-// axios.defaults.baseURL = "https://mern-music-player-server.onrender.com/api/v1";
+// axios.defaults.baseURL = "http://localhost:3000/api/v1";
+axios.defaults.baseURL = "https://mern-music-player-server.onrender.com/api/v1";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
