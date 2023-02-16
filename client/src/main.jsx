3f3df2a8a0528b6cbc18553f0193de4ba5e309f2
intelligent-psychopath <79144740+intelligent-psychopath@users.noreply.github.com>
2023-02-16 19:05:02 +0530

    Added Access Control Allow Headers & Origins

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,8 @@ import "./index.css";
 
 // axios.defaults.baseURL = "http://localhost:3000/api/v1";
 axios.defaults.baseURL = "https://mern-music-player-server.onrender.com/api/v1";
+axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
+axios.defaults.headers.common["Access-Control-Allow-Headers"] = "*";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
